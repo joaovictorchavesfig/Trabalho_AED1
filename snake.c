f131f01226d2176f12354210bb165579a7af7c3f
joaovictorchavesfig <70653905+joaovictorchavesfig@users.noreply.github.com>
2020-11-06 14:38:16 -0300

    Update snake.c

--- snake.c
+++ snake.c
@@ -6,18 +6,26 @@ int main()
     // evitando o efeito de pisca-pisca
     BITMAP *buffer = NULL;
 
-    // inicializa contagem do tempo;
+    // inicializa contagem do tempo
     srand(time(NULL));
     
     // inicializa a posição da cobra
     pont head = NULL;
-	append(&head,0,0);
+    append(&head,0,0);
     
     int dirX = 0;
     int dirY = 0;
     
-    int fruit_x = rand()%820;
-    int fruit_y = rand()%580;
+    // inicializa a primeira fruta
+    int *fruit_x;
+    int *fruit_y;
+    fruit_x = (int*)malloc(sizeof(int));
+    fruit_y = (int*)malloc(sizeof(int));
+ 	*fruit_x = rand()%820;
+	*fruit_y = rand()%580;
+	
+	// cria pontuação
+	int pontos = 0;
 
     // dimensao da cabeça da cobra
     const int tamanho = 20;
@@ -25,7 +33,7 @@ int main()
     // inicializa a biblioteca
     allegro_init();
 
-    // profundidade de cores, 32 é melhor no Win7
+    // profundidade de cores
     set_color_depth(32);
 
     // permite a leitura do teclado
@@ -44,27 +52,28 @@ int main()
         if(key[KEY_UP] || key[KEY_W]) // se pressionou para cima
         {
             dirY = -1;
-			dirX = 0; // move o quadrado para cima
+			dirX = 0; // move a cobra para cima
         }
         if(key[KEY_DOWN] || key[KEY_S]) // se pressionou para baixo
         {
             dirY = 1; 
-			dirX = 0;// move o quadrado para baixo
+			dirX = 0;// move a cobra para baixo
         }
     	if (key[KEY_LEFT] || key[KEY_A]) // se pressionou para esquerda
         {
             dirX = -1;
-			dirY = 0; // move o quadrado para a esquerda
+			dirY = 0; // move a cobra para a esquerda
         }
     	if (key[KEY_RIGHT] || key[KEY_D]) // se pressionou para direita
         {
             dirX = 1;
-			dirY = 0; // move o quadrado para a direita
+			dirY = 0; // move a cobra para a direita
         }
-		if (key[KEY_ESC])
+		if (key[KEY_ESC]) // termina o jogo
 		{
 			return 0;
 		}
+		// verifica se a cobra saiu da tela e a faz retornar do lado oposto
 		if(head->x>830)
 		{
 			head->x = 0;
@@ -92,12 +101,14 @@ int main()
         // pinta o quadrado no buffer
         rectfill(buffer, head->x, head->y, head->x + tamanho, head->y + tamanho, makecol(220, 220, 220));
 		
+		// verifica colisão da fruta com a cobra
+		pontos = pontos + eat_fruit(fruit_x,fruit_y,head->x,head->y);
+		
 		// adiciona uma fruta ao jogo
-		add_fruit(buffer,fruit_x,fruit_y);
+		add_fruit(buffer,*fruit_x,*fruit_y);
 		
         // imprimimos as coordenadas x e y no buffer para o usuário ver na tela
-        textprintf_ex(buffer, font, 10, 10, makecol(255, 0, 0), -1, "Variavel X: %d", head->x);
-        textprintf_ex(buffer, font, 10, 20, makecol(255, 0, 0), -1, "Variavel Y: %d", head->y);
+        textprintf_ex(buffer, font, 370, 10, makecol(255, 0, 0), -1, "Pontos: %d", pontos);
 
         // imprime o buffer na tela
         blit(buffer, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
