6f3271561a7966d1348bd7cd496c6ae53108b5fc
joaovictorchavesfig <70653905+joaovictorchavesfig@users.noreply.github.com>
2020-11-06 12:53:08 -0300

    Update snake.h

--- snake.h
+++ snake.h
@@ -1,46 +1,53 @@
-#ifndef TRABFINAL_H_INCLUDED
-#define TRABFINAL_H_INCLUDED
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <ncurses.h>
+#include <allegro.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define FRUIT 'o'
-#define SNAKE ' '
-#define BORDAH '|'
-#define BORDAV '_'
+//struct de lista ligada encadeada para corpo da cobra
+struct list{
+	int x;
+	int y;
+	struct list* prox;
+};
+typedef struct list list;
+typedef struct list* pont;
 
-#define SNAKE_PAIR 1
+void append(pont* head_ref, int new_x,int new_y);
+void add_fruit(BITMAP* buffer,int x,int y);
 
-void draw_board();
-
-void draw_board(){
-	
-	int x,y,maxdim;
-	
-	//Lado esquerdo
-	for(y=4;y<LINES-2;y++)
-	{
-		mvhline(y, 3, BORDAH, 1);
-	}
-	//Lado de baixo
-	for(x=4;x<COLS-6;x++)
-	{
-		mvvline(LINES-3, x, BORDAV, 1);
-	}
-	//Lado direito
-	for(y=4;y<LINES-2;y++)
-	{
-		mvhline(y, COLS-6, BORDAH, 1);
-	}
-	//Lado de cima
-	for(x=4;x<COLS-6;x++)
-	{
-		mvvline(3, x, BORDAV, 1);
-	}
-	//Contagem de pontos
-	move(2,(COLS/2)-5);
-	printw("SCORE:");
+void add_fruit(BITMAP* buffer,int x,int y)
+{
+	rectfill(buffer, x, y, x + 10, y + 10, makecol(255, 0, 0));
 }
+
+void append(pont* head_ref, int new_x,int new_y) 
+{ 
+    //alocar novo elemento
+    pont new_node = (pont) malloc(sizeof(list)); 
+  
+    pont last = *head_ref;
+   
+    new_node->x  = new_x;
+	new_node->y  = new_y;
+	
+	new_node->prox = NULL; 
+  
+	// Se a lista esta vazia
+	if (*head_ref == NULL) 
+	{ 
+       *head_ref = new_node; 
+       return; 
+    }   
+       
+	// Checa ate último elemento
+    while (last->prox != NULL) 
+        last = last->prox;
+   
+    // Muda novo elemento para último da lista
+    last->prox = new_node; 
+    return;     
+} 
 
 #endif
